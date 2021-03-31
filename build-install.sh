f0aa9107f059afb63cdb344ca1dac1d7a82175d1
Zaiqiang Dong <zqdong@nreal.ai>
2021-03-31 14:23:56 +0800

    add screen bg set

--- build-install.sh
+++ build-install.sh
@@ -1,5 +1,6 @@
 rm -rf config.h
 sudo make clean install
 mkdir -p ~/.scripts
-cp ./scrpits/* ~/.scripts
+cp -r ./scrpits ~/.config/scripts
 cp ./xinputrc ~/.xinputrc
+cp ./b.jpg ~/.config/b.jpg
--- scrpits/dwm-status.sh
+++ scrpits/dwm-status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true
 do
-    bash ~/.scripts/dwm-status-refresh.sh
+    bash ~/.config/scripts/dwm-status-refresh.sh
     sleep 1
 done
