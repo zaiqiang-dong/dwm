057f6435493c7507dea59d8c2d5d34e60520c90d
Zaiqiang Dong <zqdong@nreal.ai>
2021-03-31 09:41:23 +0800

    modify color

--- config.def.h
+++ config.def.h
@@ -15,11 +15,11 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#239B56";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_cyan, col_gray1,  col_gray2 },
 };
 
 /* tagging */
