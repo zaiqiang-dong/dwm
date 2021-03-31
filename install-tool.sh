6c79eeb0ccfc5b0d5659094482a2eb9207ee7150
Zaiqiang Dong <zqdong@nreal.ai>
2021-03-31 20:04:22 +0800

    add font

--- install-tool.sh
+++ install-tool.sh
@@ -1,3 +1,5 @@
 sudo apt install feh
 sudo apt-get install suckless-tools libx11-dev libxft-dev libxinerama-dev gcc make
 sudo apt install flameshot
+cp ./hack-font/* /usr/share/fonts/
+fc-cache -f -v
