f70aa2268b1e629428cfbb5057b768e2e1b325ed
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-01 11:57:28 +0800

    add vol command

--- config.def.h
+++ config.def.h
@@ -63,6 +63,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
+static const char *volupcmd[]  = { "amixer", "set Master 5%+" };
+static const char *voldowncmd[]  = { "amixer", "set Master 5%-"  };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
