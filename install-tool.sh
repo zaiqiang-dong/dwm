45dad4f6784ea09e0c78720e75485347c00539dc
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-02 13:38:06 +0800

    intall

--- install-tool.sh
+++ install-tool.sh
@@ -1,5 +1,6 @@
 sudo apt install feh
 sudo apt-get install suckless-tools libx11-dev libxft-dev libxinerama-dev gcc make
 sudo apt install flameshot
-cp ./hack-font/* /usr/share/fonts/
+sudo cp ./hack-font/* /usr/share/fonts/
+sudo cp ./dwm.desktop /usr/share/xsessions/
 fc-cache -f -v
