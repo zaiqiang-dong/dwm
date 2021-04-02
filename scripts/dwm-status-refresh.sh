09fddd8e246f22ea31324b9dd0e1bf10cdd114e3
Zaiqiang Dong <zqdong@nreal.ai>
2021-04-02 20:53:04 +0800

    modify status

--- scripts/dwm-status-refresh.sh
+++ scripts/dwm-status-refresh.sh
@@ -4,7 +4,7 @@ print_volume() {
     Volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
     if test "$Volume" -gt 0
     then
-	echo -e "Vol:${Volume}"
+	echo -e "\uFA7D ${Volume}"
     else
 	echo -e "Mute"
     fi
@@ -12,9 +12,32 @@ print_volume() {
 
 print_mem(){
     memfree=$(($(grep -m1 'MemAvailable:' /proc/meminfo | awk '{print $2}') / 1024))
-    echo -e "Mem:$memfree"
+    echo -e "\uF85A $memfree"
 }
 
+#load
+
+dwm_loadavg () {
+    lf=1,2,3
+    la=$(cut -d " " -f ${lf} /proc/loadavg)
+    echo -e "\uFBF2 $la"
+
+}
+
+#whether
+whether=$(curl -s wttr.in/$LOCATION?format=1 | grep -o ".[0-9].*")
+whether=`echo -e "\uFA94 $whether"`
+#network
+
+connection=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}')
+if [ "$connection" = "" ]; then
+    connection=`echo -e "\uF65A"`
+else
+    connection=`echo -e "\uFBF1"`
+fi
+
 # datetime
-DateTime=$(date +"%Y年%m月%d日 %A %T" )
-xsetroot -name "$(print_mem) $(print_volume) $DateTime "
+Date=$(date +"%Y年%m月%d日 %A" )
+Time=$(date +"%T" )
+DateTime=`echo -e "\uF073 $Date \uFBAE $Time"`
+xsetroot -name "$whether $(dwm_loadavg) $(print_mem) $(print_volume) $DateTime $connection "
