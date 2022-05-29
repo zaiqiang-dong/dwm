14eb40c9ed7dca7123a3a9a1c632e5c3b1df69b8
Zaiqiang Dong <zaiqdong@didiglobal.com>
2022-05-30 07:33:11 +0800

    add config

--- config.def.h
+++ config.def.h
@@ -47,7 +47,6 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
