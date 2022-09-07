b67f0a88f44ff7affc1723d9d1b1e283e64d4361
Zaiqiang Dong <zaiqdong@didiglobal.com>
2022-09-07 09:48:41 +0800

    add config

--- config.def.h
+++ config.def.h
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = {"1","2","3","4", "5"};
+static const char *tags[] = {"1","2","3","4","5","6"};
 
 static const Rule rules[] = {
 	/* xprop(1):
