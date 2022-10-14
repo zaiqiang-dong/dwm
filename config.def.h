94499142f89c8d3818a4b398aa7b83c766f5b7db
Zaiqiang Dong <zaiqdong@didiglobal.com>
2022-10-14 12:56:59 +0800

    add config

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = {"Hack Nerd Font Mono:style=bold:size=13" };
+static const char *fonts[]          = {"Hack Nerd Font Mono:style=bold:size=13", "Symbola:style=Regular:size=13" };
 static const char dmenufont[]       = "Hack Nerd Font Mono:size=13";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
