5c6fd40778b8c6b2ea258e22a110f8565910f7c4
Zaiqiang Dong <zaiqdong@didiglobal.com>
2022-11-29 20:04:19 +0800

    add config

--- scripts/dwm-status-refresh.sh
+++ scripts/dwm-status-refresh.sh
@@ -41,9 +41,9 @@ get_battery_charging_status() {
 
 	if $(acpi -b | grep --quiet Discharging)
 	then
-		echo -e "\uF578";
+        echo -e "\ufd0a"
 	else # acpi can give Unknown or Charging if charging, https://unix.stackexchange.com/questions/203741/lenovo-t440s-battery-status-unknown-but-charging
-		echo -e "\uF583";
+		echo -e "\ue780";
 	fi
 }
 
