08921ed4d1503e7077389c04f7f54c004bc96f99
Zaiqiang Dong <zaiqdong@didiglobal.com>
2023-01-05 19:38:07 +0800

    add config

--- dwm-lock.sh
+++ dwm-lock.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-i3lock -c 000000 -e
-xset dpms force off
+ps -aux | grep "xscreensaver" | grep -v "grep" | awk '{print $2}' | while read line;
+do
+	kill -9 $line
+done
 
+xset dpms force off
+i3lock -c 000000 -e
+xscreensaver &
