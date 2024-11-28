f2a856b0cabf6c7e85e63995ae2ddf1afc075593
Zaiqiang Dong <zaiqdong@didiglobal.com>
2024-11-28 09:46:33 +0800

    add config

--- scripts/dwm-start-tools.sh
+++ scripts/dwm-start-tools.sh
@@ -3,9 +3,10 @@ feh --bg-scale /home/$USER/.config/b.jpg
 picom -b --config=/home/zaiqdong/.config/picom-config
 alsactl --file ~/.config/asound.state restore
 xbindkeys
-blueman-tray
+# blueman-tray
+blueman-applet &
 fcitx5 &
-clash-verge &
+# clash-verge &
 bytedance-feishu-stable &
 #google-chrome-stable --proxy-server="http://127.0.0.1:15732"
 google-chrome-stable &
