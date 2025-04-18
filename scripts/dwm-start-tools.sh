8b5c51e86a2e49fda0f47da58c4e388ee4860097
Zaiqiang Dong <dongzq@autoai.com>
2025-04-18 10:50:55 +0800

    add config

--- scripts/dwm-start-tools.sh
+++ scripts/dwm-start-tools.sh
@@ -4,7 +4,6 @@ feh --bg-scale /home/$USER/.config/b.jpg
 picom -b --config=/home/zaiqdong/.config/picom-config
 alsactl --file ~/.config/asound.state restore
 xbindkeys
-# copyq &
 # blueman-tray
 ./dwm-sync-clipboard.sh &
 blueman-applet &
