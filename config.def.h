2480263a5d1bd5d6708c794a97ea27735048df79
Zaiqiang Dong <dongzq@autoai.com>
2025-04-28 17:45:36 +0800

    add config

--- config.def.h
+++ config.def.h
@@ -15,16 +15,16 @@ static const int focusonwheel       = 0;
 #ifdef IS_4K
 static const unsigned int systrayiconsize = 40;
 static const char *fonts[] = {
+    "Alibaba PuHuiTi 3.0 105 Heavy:style=105 Heavy,Regular:size=24",
+    "JetBrains Mono NL:style=Bold:size=24",
+    "Noto Color Emoji:style=Regular:size=20",
+    "JetBrainsMonoNL Nerd Font:style=Bold:size=24",
     /*
-     * "JetBrains Mono NL:style=Bold:size=24",
-     * "Noto Color Emoji:style=Regular:size=20",
-     * "JetBrainsMonoNL Nerd Font:style=Bold:size=24",
-     * "Alibaba PuHuiTi 3.0 105 Heavy:style=105 Heavy,Regular:size=22",
+     * "Alibaba PuHuiTi 3.0 105 Heavy:style=105 Heavy,Regular:size=16",
+     * "JetBrains Mono NL:style=Bold:size=16",
+     * "Noto Color Emoji:style=Regular:size=12",
+     * "JetBrainsMonoNL Nerd Font:sytle=Bold:size=16",
      */
-    "JetBrains Mono NL:style=Bold:size=16",
-    "Noto Color Emoji:style=Regular:size=12",
-    "JetBrainsMonoNL Nerd Font:sytle=Bold:size=16",
-    "Alibaba PuHuiTi 3.0 105 Heavy:style=105 Heavy,Regular:size=14",
 };
 #else
 static const unsigned int systrayiconsize = 20;
